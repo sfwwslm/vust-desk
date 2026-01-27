32d35d3024e737d649d18bfe53031c0b01c2bdb8
sfwwslm <sfwwslm@gmail.com>
2026-01-27 20:33:38 +0800

    feat(constants): 添加GitHub仓库链接常量

---
+++ src/constants/links.ts
@@ -0,0 +1,3 @@
+export const GITHUB_REPO_URL = "https://github.com/sfwwslm/vust-desk";
+export const GITHUB_ISSUES_URL = `${GITHUB_REPO_URL}/issues`;
+export const GITHUB_LATEST_RELEASE_URL = `${GITHUB_REPO_URL}/releases/latest`;
